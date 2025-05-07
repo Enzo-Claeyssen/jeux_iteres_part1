561211448c63c96a9cb29594dd916f94a45d0572
Enzo Claeyssen <enzo.claeyssen@gmail.com>
2025-05-07 12:07:15 +0200

    Updated both algorithms to return Prod rewards too

--- DQN.py
+++ DQN.py
@@ -6,58 +6,74 @@ from stable_baselines3.common.callbacks import BaseCallback
 
 # === CALLBACK POUR LOGGER LES RÉCOMPENSES ===
 class RewardLogger(BaseCallback):
-    def __init__(self, verbose=0):
+    def __init__(self, env, verbose=0, useProdForReward = False):
         super().__init__(verbose)
-        self.episode_rewards = []
+        self.env = env
+        self.train_episode_rewards = []
+        self.prod_episode_rewards = []
         self.current_rewards = 0
+        self.useProdForReward = useProdForReward
 
     def _on_step(self):
         self.current_rewards += self.locals["rewards"][0]
         if self.locals["dones"][0]:
-            self.episode_rewards.append(self.current_rewards)
+            if self.useProdForReward :
+                self.prod_episode_rewards.append(test_dqn(self.env, self.locals['self'], maxTimesteps = 100, render = False))
+            self.train_episode_rewards.append(self.current_rewards)
             self.current_rewards = 0
         return True
 
 # === ENTRAÎNEMENT DQN ===
-def train_dqn(env, lr = 0.01, buffer_size = 500, 
-              exploration_fraction = 0.3, exploration_final_eps = 0.05, 
-              timesteps = 2000):
+def train_dqn(env, lr = 0.01, gamma = 0.99, buffer_size = 500, update_freq = 10,
+              exploration_initial_eps = 1, exploration_fraction = 0.3, exploration_final_eps = 0.05, 
+              timesteps = 2000, useProdForReward = False):
     
+    logger = RewardLogger(env, useProdForReward = useProdForReward)
     env = make_vec_env(lambda: env, n_envs=1)
-    logger = RewardLogger()
 
     model = DQN(
         "MlpPolicy",
         env,
         verbose=0,
         learning_rate=lr,
+        gamma = gamma,
         buffer_size=buffer_size,
         train_freq=1,
-        target_update_interval=10,
+        target_update_interval=update_freq,
+        exploration_initial_eps = exploration_initial_eps,
         exploration_fraction=exploration_fraction,
         exploration_final_eps=exploration_final_eps,
     )
 
     model.learn(total_timesteps=timesteps, callback=logger)
 
-    return model, logger.episode_rewards
+    return model, logger.train_episode_rewards, logger.prod_episode_rewards
 
 # === TEST DU MODÈLE ===
-def test_dqn(env, model, maxTimesteps = 20):
+def test_dqn(env, model, maxTimesteps = 20, render = True):
     preventInfinite = maxTimesteps
     env = make_vec_env(lambda: env, n_envs=1)
     obs = env.reset()
     trajectory = [obs]
     done = False
+    total_reward = 0
+    
     while not done and preventInfinite > 0 :
         preventInfinite -= 1
         action, _ = model.predict(obs, deterministic=True)
         obs, reward, done, info = env.step(action)
+        total_reward += reward
         if done :
             trajectory.append(info[0]["terminal_observation"])
         else :
             trajectory.append(obs)
-        env.render("human")
+        
+        if render :
+            env.render("human")
+        
         if done:
             break
-    print("Trajectoire :", " → ".join(map(str, trajectory)))
+    
+    if render :
+        print("Trajectoire :", " → ".join(map(str, trajectory)))
+    return total_reward
--- QLearning.py
+++ QLearning.py
@@ -2,42 +2,47 @@ import numpy as np
 from math import exp
 
 
-def train_q_learning(env, alpha = 0.1, gamma = 0.9, epsilon = 0.1, timesteps = 2000, useProdForReward = False) :
+def train_q_learning(env, alpha = 0.1, gamma = 0.9,
+                     eps_start = 0.9, eps_end = 0.05, eps_fraction = 0.3,
+                     timesteps = 2000, useProdForReward = False) :
     n_states = env.observation_space.n
     n_actions = env.action_space.n
     Q = np.zeros((n_states, n_actions))
-    rewards = []
+    trainRewards = []
+    prodRewards = []
     
     state = env.reset()
     episode_reward = 0
     done = False
     
-    while timesteps > 0 :	# Continue until reached amount of timesteps
+    for i in range(timesteps) :	# Continue until reached amount of timesteps
         if done :	# If episode ended
             if useProdForReward :
-                episode_reward = test_q_learning(env, Q, render = False)   #Uses prod model to report rewards
-            rewards.append(episode_reward)
+                prodRewards.append(test_q_learning(env, Q, render = False))   #Uses prod model to report rewards
+            trainRewards.append(episode_reward)
             state = env.reset()
             episode_reward = 0
             done = False
+            
+        
+        # Décroissance de epsilon
+        epsilon = get_new_epsilon(eps_start, eps_end, eps_fraction, (1-i/timesteps))
+        
+        # Choix de l'action
+        if np.random.rand() < epsilon :
+            action = np.random.choice(range(n_actions))
         else :
-            timesteps -= 1
+            action = np.argmax(Q[state])
             
-            # Choix de l'action
-            if np.random.rand() < epsilon :
-                action = np.random.choice(range(n_actions))
-            else :
-                action = np.argmax(Q[state])
+        # On applique l'action choisie
+        next_state, reward, done, _ = env.step(action)
             
-            # On applique l'action choisie
-            next_state, reward, done, _ = env.step(action)
+        # Mise à jour de la table
+        Q[state, action] += alpha * (reward + gamma * np.max(Q[next_state]) - Q[state, action])
             
-            # Mise à jour de la table
-            Q[state, action] += alpha * (reward + gamma * np.max(Q[next_state]) - Q[state, action])
-            
-            state = next_state
-            episode_reward += reward
-    return Q, rewards
+        state = next_state
+        episode_reward += reward
+    return Q, trainRewards, prodRewards
 
 
 def test_q_learning(env, QTable, maxTimesteps = 100, render = True) :
@@ -53,3 +58,16 @@ def test_q_learning(env, QTable, maxTimesteps = 100, render = True) :
         if render : env.render()
         total_reward += reward
     return total_reward
+
+
+def get_new_epsilon(start, end, fraction, progress_remaining) :
+    """
+    start : starting epsilon value
+    end : last epsilon value
+    fraction : if 0.1, then after 10% of training completed epsilon should be equal to end
+    progress_remaing : % / 100 of progress remaining (0 if ended, 1 if starting)
+    """
+    if (1 - progress_remaining) > fraction :
+        return end
+    else :
+        return start + (1 - progress_remaining) * (end - start) / fraction
